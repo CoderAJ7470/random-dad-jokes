1ab136978c089070be58783a18bce251a437eb57
Ameya Joshi <ameyasureshjoshi@gmail.com>
2026-05-21 16:13:50 -0500

    One last minor change

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ export const Header = () => {
     <p>
       This page shows dad jokes. Jokes have been pulled from the{' '}
       <a href='https://dadjokes.bamboozledaardvark.com/' target='_blank'>
-        dad jokes API
+        Dad Joke API
       </a>
       &nbsp;and are not the responsibility of this application's author.
     </p>
