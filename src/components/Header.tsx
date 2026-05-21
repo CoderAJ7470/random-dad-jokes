e57d457bc3c302e80f13c609d5be3832d9959f12
Ameya Joshi <ameyasureshjoshi@gmail.com>
2026-05-21 16:09:53 -0500

    Changed the page link for the description in the Header and re-worded the dsiclaimer

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,11 @@
 export const Header = () => {
   return (
     <p>
-      This simple page shows dad jokes. Jokes have been pulled from the{' '}
-      <a href='https://dadjokes.bamboozledaardvark.com/api' target='_blank'>
+      This page shows dad jokes. Jokes have been pulled from the{' '}
+      <a href='https://dadjokes.bamboozledaardvark.com/' target='_blank'>
         dad jokes API
       </a>
-      and are not the responsibility of this application's author.
+      &nbsp;and are not the responsibility of this application's author.
     </p>
   );
 };
